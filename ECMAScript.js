3e48ab17e1a0d5bb45b07f9a04cae26da9659449
Senay Yakut <senaykt@gmail.com>
2019-03-22 15:11:58 -0700

    ECMAScript6 changes

---
+++ ECMAScript.js
@@ -0,0 +1,14 @@
+//It is also know as ES2015.
+//The changes add a lot of syntactic sugar that allow developers to create applications in an object oriented style.
+
+/*A lot of new syntax features were introduced including:
+    classes,
+    modules,
+    templating,
+    for/of loops,
+    generator expressions,
+    arrow functions,
+    collections,
+    promises.*/
+/*Frequently new features arrive that are part of the successor ES7. A common way is to translate modern JavaScript (ES6, ES7 and other experimental proposals) to ES5. This makes sure that also old browsers can execute the code. There are tools like Babel that transforms new JavaScript to ES5.*/
+/*Besides syntactic sugar coming from ECMAScript standards there are features that require a Polyfill. Usually they are necessary because whole class/method implementations were added to the standard.*/
