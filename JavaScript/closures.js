c3cb1ffaa4bd264de67c77ded130f3535fe70cb4
Senay Yakut <senaykt@gmail.com>
2019-03-30 14:39:25 -0700

    call(),apply(),bind()

--- JavaScript/closures.js
+++ JavaScript/closures.js
@@ -19,3 +19,40 @@ cat.printInfo(); //prints Name: window Color: undefined Age: undefined
 
 //Why are color and age undefined in line 2? You might be thinking, “the cat object properties are clearly defined above and are in an outer more global scope aren’t they?” More importantly, where did “window” come from?
 //JavaScript loses scope of this when used inside of a function that is contained inside of another function.When it is lost, by default, this will be bound to the global window object.In our example, it just so happens that the window object also has a “name” property with a value of “window”.
+
+var cat = {
+    name: "Gus",
+    color: "gray",
+    age: 15,
+
+    printInfo: function () {
+        var that = this;
+        console.log("Name:", this.name, "Color:", this.color, "Age:", this.age); //prints correctly
+
+        nestedFunction = function () {
+            console.log("Name:", that.name, "Color:", that.color, "Age:", that.age); //prints correctly
+        }
+        nestedFunction();
+    }
+}
+cat.printInfo();
+//Because we bound this to a variable that, it will be available just like any other variable
+
+var cat = {
+    name: "Gus",
+    color: "gray",
+    age: 15,
+
+    printInfo: function () {
+        console.log("Name:", this.name, "Color:", this.color, "Age:", this.age);
+        nestedFunction = function () {
+            console.log("Name:", this.name, "Color:", this.color, "Age:", this.age);
+        }
+        nestedFunction.call(this);
+        nestedFunction.apply(this);
+
+        var storeFunction = nestedFunction.bind(this);
+        storeFunction();
+    }
+}
+cat.printInfo();
