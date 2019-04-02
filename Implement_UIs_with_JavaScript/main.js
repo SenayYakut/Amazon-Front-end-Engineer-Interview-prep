98208412bcc97700d49dd591a6e140fcb9734c87
Senay Yakut <senaykt@gmail.com>
2019-04-02 13:25:44 -0700

    implementing the user interface

---
+++ Implement_UIs_with_JavaScript/main.js
@@ -0,0 +1,71 @@
+function fetchIssues() {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">' +
+            '<h6>Issue ID: ' + id + '</h6>' +
+            '<p><span class="label label-info">' + status + '</span></p>' +
+            '<h3>' + desc + '</h3>' +
+            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
+            '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
+            '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a> ' +
+            '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a>' +
+            '</div>';
+    }
+}
+
+function saveIssue(e) {
+    var issueId = chance.guid();
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueStatus = 'Open';
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') === null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    document.getElementById('issueInputForm').reset();
+
+    fetchIssues();
+
+    e.preventDefault();
+}
+
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = "Closed";
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
+
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
