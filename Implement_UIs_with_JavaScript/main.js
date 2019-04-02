69c39bed755ff4f312b71c35be5baa3faf25396e
Senay Yakut <senaykt@gmail.com>
2019-04-02 14:32:19 -0700

    app with pure JavaScript

--- Implement_UIs_with_JavaScript/main.js
+++ Implement_UIs_with_JavaScript/main.js
@@ -1,28 +1,3 @@
-function fetchIssues() {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesList = document.getElementById('issuesList');
-
-    issuesList.innerHTML = '';
-
-    for (var i = 0; i < issues.length; i++) {
-        var id = issues[i].id;
-        var desc = issues[i].description;
-        var severity = issues[i].severity;
-        var assignedTo = issues[i].assignedTo;
-        var status = issues[i].status;
-
-        issuesList.innerHTML += '<div class="well">' +
-            '<h6>Issue ID: ' + id + '</h6>' +
-            '<p><span class="label label-info">' + status + '</span></p>' +
-            '<h3>' + desc + '</h3>' +
-            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
-            '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
-            '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a> ' +
-            '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a>' +
-            '</div>';
-    }
-}
-
 function saveIssue(e) {
     var issueId = chance.guid();
     var issueDesc = document.getElementById('issueDescInput').value;
@@ -54,6 +29,32 @@ function saveIssue(e) {
     e.preventDefault();
 }
 
+
+function fetchIssues() {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">' +
+            '<h6>Issue ID: ' + id + '</h6>' +
+            '<p><span class="label label-info">' + status + '</span></p>' +
+            '<h3>' + desc + '</h3>' +
+            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
+            '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
+            '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a> ' +
+            '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a>' +
+            '</div>';
+    }
+}
+
 function setStatusClosed(id) {
     var issues = JSON.parse(localStorage.getItem('issues'));
 
@@ -67,5 +68,3 @@ function setStatusClosed(id) {
 
     fetchIssues();
 }
-
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
